29297c95b465d295efd9a7ccee6392b31c7f3123
Ryan Kelley <rtkelley@gmail.com>
2017-08-10 15:28:12 -0400

    updated template literal and added push to results inner HTML

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 // API key: mQ4ZzCsN26NvGKVS
 
 
+//declaring global variables
+const header = document.querySelector(header);
+const contentWrapper = document.getElementById("content-wrapper");
 
+//below needs to go into for-loop to iterate over API data when we have it
+
+//creating template literal variables
 let dateDay;
 let title;
 let venue;
 let time;
 let location;
+let genre;
 let description;
 
 
-
+//creating template literal for results HTML
 let resultsHTML = `
   <div class="results-wrapper">
     <div class="date-box">
@@ -26,7 +33,10 @@ let resultsHTML = `
         <p class="location">${location}</p>
       </div>
       <div class="description-info">
-        <p class="description">${description}</p>
+        <p class="description">${genre}: ${description}</p>
       </div>
     </div>
   </div>`
+
+//adding template literal to HTML
+contentWrapper.innerHTML += resultsHTML;
